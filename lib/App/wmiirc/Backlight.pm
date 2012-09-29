8fdfbb45735cb0de1ac5b23ba1b87e3d2986399b
David Leadbeater <dgl@dgl.cx>
2012-09-29 14:50:28 +0100

    MacBook Air keyboard backlight
    (In a rather hacky way, probably should use pommed.)

--- lib/App/wmiirc/Backlight.pm
+++ lib/App/wmiirc/Backlight.pm
@@ -14,4 +14,12 @@ sub key_backlight_up(XF86MonBrightnessUp) {
   system qw(xbacklight -steps 1 -time 0 -inc 10);
 }
 
+sub key_kbd_backlight_down(XF86KbdBrightnessDown) {
+  system qw(gksudo -- sh -c), "echo 0 > /sys/devices/platform/applesmc.768/leds/smc::kbd_backlight";
+}
+
+sub key_kbd_backlight_up(XF86KbdBrightnessUp) {
+  system qw(gksudo -- sh -c), "echo 4 > /sys/devices/platform/applesmc.768/leds/smc::kbd_backlight";
+}
+
 1;
